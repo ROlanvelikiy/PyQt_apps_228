1fb64470e99b26fe110f18af2c4660631e16e788
R0N1N32 <bezdushniy.main@gmail.com>
2024-02-28 22:02:22 +0600

    черепаха

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import turtle
 
+turtle.speed(10)
+
 turtle.begin_fill()
 for i in range(4):
     turtle.forward(90)
     turtle.left(90)
 turtle.end_fill()
+turtle.circle(100)
+turtle.hideturtle()
 turtle.exitonclick()
